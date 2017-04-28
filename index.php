d3acbff6cfa4a482a6b1a176e269a35ce0b29645
nobe1996 <rozella1337@gmail.com>
2017-04-28 12:25:14 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 $tns = "
 (DESCRIPTION =
     (ADDRESS_LIST =
-      (ADDRESS = (PROTOCOL = TCP)(HOST = 127.0.0.1)(PORT = 1521))
+      (ADDRESS = (PROTOCOL = TCP)(HOST = http://imgboard.irinyi.cloud/)(PORT = 1521))
     )
     (CONNECT_DATA =
       (SID = kabinet)
