722750ebf6c1574badc4bf03b32fe2a5ac00f492
nobe1996 <rozella1337@gmail.com>
2017-05-03 11:29:09 +0200

    asd

--- login.php
+++ login.php
@@ -15,6 +15,7 @@ $tns = "
   )";
   
 $conn = oci_connect('h669113', 'h669113', $tns,'UTF8') or die();
+
 if (!$conn) {
     $e = oci_error();
     trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
