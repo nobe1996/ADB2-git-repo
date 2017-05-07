36644fdb59ded85fa9e3e3afffbb0460d42e66b7
nobe1996 <rozella1337@gmail.com>
2017-05-07 16:11:03 +0200

    asd

--- bejelentkezes.php
+++ bejelentkezes.php
@@ -11,6 +11,7 @@ Username: <input type="text" name="username"><br>
 Password: <input type="password" name="password"><br>
 <input type="submit">
 </form>
+<a href="registarion.php">Regisztráció</a>
 </div>
 
 </body>
