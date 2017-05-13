da07f9ed6da1ffe03e0b37637276e2caaf2f855d
nobe1996 <rozella1337@gmail.com>
2017-05-13 11:00:35 +0200

    Add files via upload

--- signup.php
+++ signup.php
@@ -31,19 +31,6 @@ if (isset($_POST['signup'])){
 }
 $_POST = array();
 ?>
-<div id="login" class="login">
-	<?php if ($message != ""){?>
-			<p><?php echo $message;?></p>
-	<?php 
-	}
-	?>
-	<form method="post" action="signup.php">
-		Username:<input type="text" name="username" value=""><br>
-		Password:<input type="password" name="password" value=""><br>
-		<input type="submit" name="send" value="Bejelentkezés"/>
-	</form>
-</div>
-
 <div class="login">
 	<?php if ($message != ""){?>
 			<p><?php echo $message;?></p>
@@ -51,10 +38,10 @@ $_POST = array();
 	}
 	 ?>
 	<form method="post" action="signup.php">
-		Felhasználónév:*<input type="text" name="username" value="">
-		Név:*<input type="text" name="nev" value="">
-		Jelszó:* (legalább 8 karakter)<input type="password" name="pass" value="">
-		Jelszó mégegyszer:*<input type="password" name="passwordagain" value="">
+		Felhasználónév:*<input type="text" name="username" value=""><br>
+		Név:*<input type="text" name="nev" value=""><br>
+		Jelszó:* (legalább 8 karakter)<input type="password" name="pass" value=""><br>
+		Jelszó mégegyszer:*<input type="password" name="passwordagain" value=""><br>
 		Csoport:* 
 		<select name="groups">
 			<option value="" disabled selected>Válassz csoportot.</option>
@@ -66,19 +53,19 @@ $_POST = array();
 						echo '<option value="'. $row["CS_NEV"] . '">'.$row["CS_NEV"] .'</option>'; 
 					} 
 				?>
-		</select>
+		</select><br>
 		Lakhely:*
 		<select name="hometown">
 				<option value="" disabled selected>Válassz lakhelyt.</option>
 				<?php
-					$stid = oci_parse($conn, "SELECT HELY_ID, ORSZAG, MEGYE, TELEPULES FROM CSOPORTOK;");
+					$stid = oci_parse($conn, "SELECT HELY_ID, ORSZAG, MEGYE, TELEPULES FROM HELYEK;");
 					$r = oci_execute($stid);
 
 					while ($row = oci_fetch_assoc($stid)) { 
 						echo '<option value="'. $row["HELY_ID"] . '">'.$row["ORSZAG"] .','.$row["MEGYE"] .','.$row["TELEPULES"] .'</option>'; 
 					} 
 				?>
-		</select>
+		</select><br>
 		<input type="submit" name="signup" value="Regisztráció">
 	</form>
 </div>
