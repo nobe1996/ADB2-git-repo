64d7fee393e655ff69bc565cdbcc040304cf70b0
nobe1996 <rozella1337@gmail.com>
2017-05-13 15:47:51 +0200

    Add files via upload

--- upload.php
+++ upload.php
@@ -42,66 +42,70 @@ if(isset($_POST['submit'])){
 			oci_execute($stmt);
 			oci_fetch($stmt);
 			
-			
 			$temp = explode(".", $_FILES["fileToUpload"]["name"]);
 			$newfilename = ($number_of_pictures+1). '.' . end($temp);
 			$target_dir = "images/";
-			
 			$target_file = $target_dir . $newfilename;
-			
 			$uploadOk = 1;
 			$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 
 			if(isset($_POST["submit"])) {
 				$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
 				if($check !== false) {
-					$message= "A fájl kép - " . $check["mime"] . ".";
 					$uploadOk = 1;
 				} else {
-					$message= "A fájl nem kép.";
 					$uploadOk = 0;
 				}
 			}
-
 			if (file_exists($target_file)) {
-				$message= "Már létezik ilyen fájl!";
 				$uploadOk = 0;
 			}
-
 			if ($_FILES["fileToUpload"]["size"] > 50000000) {
-				$message= "A fájl mérete túl nagy.";
 				$uploadOk = 0;
 			}
-
 			if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 			&& $imageFileType != "gif" ) {
-				$message= "Elnézést, csak JPG, JPEG, PNG & GIF fájl típusok engedélyezettek.";
 				$uploadOk = 0;
 			}
-
-			if ($uploadOk == 0) {
-				$message= "A fájl nem lett feltöltve.";
-
-			} else {
+			if (($uploadOk == 1) && ($_POST['location'] != '') && ($_POST['categories'] != '') ) {
 				if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-					$message= "A ". $newfilename . " nevű fájl feltöltve.";
+					$values = "'".$target_file."','".$_SESSION['login_name']."','".htmlspecialchars($_POST["location"])."','".htmlspecialchars($_POST["categories"])."'";
+				
+					$stid = oci_parse($conn, 'INSERT INTO KEPEK (URL, FELHASZNALONEV, HELY_ID, KAT_NEV) VALUES ('.$values.')');
+					oci_execute($stid);
 					header("Location: indexpage.php");
-					
-					
 				}
-			}
+			} 
 
 }
 ?>
-	<?php if ($message != ""){?>
-			<p><?php echo $message;?></p>
-	<?php 
-	}
-	?>
+
 
 	<form action="upload.php" method="post" enctype="multipart/form-data" align="center">
             Kép feltöltése: <input type="file" name="fileToUpload">
 			Kép készítésének helye:
+			<select name="location">
+				<option value="" disabled selected>Válassz kép készítésének helyét.</option>
+				<?php
+					$stid1 = oci_parse($conn, "SELECT HELY_ID, ORSZAG, MEGYE, TELEPULES FROM HELYEK");
+					oci_execute($stid1);
+					
+					while ($row = oci_fetch_assoc($stid1)) { 
+						echo '<option value="'. $row["HELY_ID"] . '">'.$row["ORSZAG"] .','.$row["MEGYE"] .','.$row["TELEPULES"] .'</option>'; 
+					} 
+				?>
+			</select><br>
 			Kategória:
+			<select name="categories">
+				<option value="" disabled selected>Válassz kategóriát.</option>
+				<?php
+					$stid1 = oci_parse($conn, "SELECT KAT_NEV FROM KATEGORIAK");
+					oci_execute($stid1);
+					
+					while ($row = oci_fetch_assoc($stid1)) { 
+						echo '<option value="'. $row["KAT_ID"] . '">'.$row["KAT_NEV"] .'</option>'; 
+					} 
+				?>
+			</select><br>
             <input type="submit" value="Feltoltes" name="submit">
     </form>
