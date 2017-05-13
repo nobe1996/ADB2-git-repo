bb432fda69df33389009fb6815c4c258dfb2c9be
nobe1996 <rozella1337@gmail.com>
2017-05-13 16:12:36 +0200

    Add files via upload

--- upload.php
+++ upload.php
@@ -2,41 +2,6 @@
 include_once('dbconnect.php');
 $message = "";
 if(isset($_POST['submit'])){
-	/*$username = $_SESSION["login_name"];
-	
-	$stid = oci_parse($conn, "SELECT FELHASZNALONEV, JELSZO FROM FELHASZNALOK WHERE FELHASZNALONEV LIKE '" . $username ."'");
-	if (!$stid) {
-		$e = oci_error($conn);
-		trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
-	}
-	$r = oci_execute($stid);
-	if (!$r) {
-		$e = oci_error($stid1);
-		trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
-	}
-	$stmt= oci_parse($conn, "SELECT COUNT(FELHASZNALONEV) AS NUMBER_OF_ROWS FROM FELHASZNALOK WHERE FELHASZNALONEV LIKE '" . $username ."'");
-	oci_define_by_name($stmt, 'NUMBER_OF_ROWS', $number_of_rows);
-	oci_execute($stmt);
-	oci_fetch($stmt);
-	if ($username != "" && $jelszo != "") {
-		if ($number_of_rows == '0'){
-			$message = "A felhasználó nem létezik!";
-		} else {
-			$row = oci_fetch_assoc($stid);		
-			if ($jelszo != $row['JELSZO']){
-				$message = "A megadott jelszó nem helyes!";
-			} else {
-				$_SESSION['login'] = true;
-				$_SESSION['login_name'] = $row['FELHASZNALONEV'];
-				$_POST = array();
-				header("Location: indexpage.php");
-			}
-		}
-	}
-	$_POST = array();*/
-	
-	
-			$message='';
 			$stmt= oci_parse($conn, "SELECT COUNT(URL) AS NUMBER_OF_PICTURES FROM KEPEK");
 			oci_define_by_name($stmt, 'NUMBER_OF_PICTURES', $number_of_pictures);
 			oci_execute($stmt);
@@ -49,20 +14,16 @@ if(isset($_POST['submit'])){
 			$uploadOk = 1;
 			$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 			
-			if(isset($_POST["submit"])) {
-				$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-				if($check !== false) {
-					$uploadOk = 1;
-				} else {
-					$uploadOk = 0;
-				}
+			$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+			if($check !== false) {
+				$uploadOk = 1;
+			} else {
+				$uploadOk = 0;
 			}
-
 			if ($_FILES["fileToUpload"]["size"] > 50000000) {
 				$uploadOk = 0;
 			}
-			if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-			&& $imageFileType != "gif" ) {
+			if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
 				$uploadOk = 0;
 			}
 			if (($uploadOk == 1) && ($_POST['location'] != '') && ($_POST['categories'] != '') ) {
@@ -71,17 +32,18 @@ if(isset($_POST['submit'])){
 					echo "siker";
 					$stid = oci_parse($conn, 'INSERT INTO KEPEK (URL, FELHASZNALONEV, HELY_ID, KAT_NEV) VALUES ('.$values.')');
 					oci_execute($stid);
+					$_POST = array();
 					header("Location: indexpage.php");
 				}
 			}
-			//header("Location: indexpage.php");
 
-}
+			$_POST = array();
+}	
 ?>
 
 
-	<form action="indexpage.php" method="post" enctype="multipart/form-data" align="center">
-            Kép feltöltése: <input type="file" name="fileToUpload">
+	<form action="upload.php" method="post" enctype="multipart/form-data" align="center">
+            Kép feltöltése: <input type="file" name="fileToUpload"><br>
 			Kép készítésének helye:
 			<select name="location">
 				<option value="" disabled selected>Válassz kép készítésének helyét.</option>
